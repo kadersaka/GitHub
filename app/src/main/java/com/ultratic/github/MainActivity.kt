8450c835b45bde420e223981e61ae77849e21443
ANSLABS <kadersaka@gmail.com>
2019-09-04 09:10:53 +0100

    MIse à jour de Main,, deux fichiers

--- app/src/main/java/com/ultratic/github/MainActivity.kt
+++ app/src/main/java/com/ultratic/github/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ultratic.github
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //first appp with GitHub
+        val btn = findViewById<Button>(R.id.btn)
+        val text = findViewById<TextView>(R.id.text)
     }
 }
